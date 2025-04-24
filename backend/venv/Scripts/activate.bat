5c43de1cf3a415901287047392a18943c6831701
TCBaker99 <tcassbaker@gmail.com>
2025-04-24 18:35:33 +1200

    Reinstalled repo

--- backend/venv/Scripts/activate.bat
+++ backend/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Itstr\OneDrive\Documents\Artificial-Intelligence-A1\backend\venv
+set VIRTUAL_ENV=C:\Users\Itstr\OneDrive\Documents\AI_Assignment\AUT-COMP717-AI-Assignment_1\backend\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
